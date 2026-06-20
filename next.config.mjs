8277d891e1cb195016891b29f9986642ac490354
williamralloma <williamralloma@users.noreply.github.com>
2026-06-20 12:24:52 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const repo = 'william-ralloma-portfolio';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
